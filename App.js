847c9f836ce85a82e002b2c6bddbca65ca176dc6
Jaison <jaisonschmidt@gmail.com>
2024-05-29 21:06:26 -0300

    Corrige bug de visualizacao da camera

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const [permission, requestPermission] = useCameraPermissions();
+  const [facyng, setFacyng] = useState('back');
 
   if( !permission ) {
     // nem sei se tenho permissão de acesso a camera
@@ -26,8 +28,9 @@ export default function App() {
   // temos permissão para acessar a camera
   return (
     <View style={styles.container}>
-      <Text>Temos permissão para acessar a camera</Text>
-      <StatusBar style="auto" />
+      <CameraView style={styles.camera} facyng={facyng}>
+        <View style={styles.buttonContainer}></View>
+      </CameraView>
     </View>
   );
 }
@@ -35,8 +38,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center', REMOVER ESSA INSTRUÇÃO
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    backgroundColor: 'transparent',
+    flex: 1,
+  }
 });
